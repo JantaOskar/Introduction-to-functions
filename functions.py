83990d39ad0e740c041d12edd263fa2bb5ccab41
Oskar <jantaoskar@gmail.com>
2022-12-17 00:08:35 +0100

    2022_12_17_01

--- functions.py
+++ functions.py
@@ -18,17 +18,21 @@ def palindrome_sentence(sentence):
     # return string[::-1].casefold() == string.casefold()
     return is_palindrome(string)
 
-sentence_input = input("Please enter a sentence to check: ")
-if palindrome_sentence(sentence_input):
-    print("'{}' is a palindrome".format(sentence_input))
-else:
-    print("'{}' is not a palindrome".format(sentence_input))
 
-word = input("Please enter a word to check: ")
-if is_palindrome(word):
-    print("'{}' is a palindrome".format(word))
-else:
-    print("'{}' is not a palindrome".format(word))
+# sentence_input = input("Please enter a sentence to check: ")
+# if palindrome_sentence(sentence_input):
+#     print("'{}' is a palindrome".format(sentence_input))
+# else:
+#     print("'{}' is not a palindrome".format(sentence_input))
+#
+# word = input("Please enter a word to check: ")
+# if is_palindrome(word):
+#     print("'{}' is a palindrome".format(word))
+# else:
+#     print("'{}' is not a palindrome".format(word))
+
+answer = multiply(18, 3)
+print(answer)
 
 #
 # answer = multiply(10.5, 4)
