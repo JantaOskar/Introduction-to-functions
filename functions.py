449be54e42244eb2786fd6c9db6857148cecea65
Oskar <jantaoskar@gmail.com>
2022-12-17 23:14:38 +0100

    2022_12_17_02

--- functions.py
+++ functions.py
@@ -1,15 +1,38 @@
 def multiply(x, y):
+    """
+    Multiply two numbers.
+    :param x: The first number to multiply.
+    :param y: The number to multiply `x` by.
+    :return: The product of `x` and `y`
+    """
     result = x * y
     return result
 
 
 def is_palindrome(string):
+    """
+    Check if a `string` is a palindrome.
+
+    The function accepts only alphanumeric characters in a string.
+
+    :param string: The String to be checked.
+    :return: True if palindrome, False otherwise.
+    """
     # backwards = string[::-1]
     # return backwards == string
     return string[::-1].casefold() == string.casefold()
 
 
 def palindrome_sentence(sentence):
+    """
+    Check if a `string` is a palindrome.
+
+    The function ignores whitespace, capitalistaion and
+    punctuation in the sentence.
+
+    :param sentence: The String to be checked.
+    :return: True if palindrome, False if not a palindrome.
+    """
     string = ""
     for char in sentence:
         if char.isalnum():
