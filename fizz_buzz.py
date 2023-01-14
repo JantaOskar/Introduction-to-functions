d7544a4c28b6a1c6a3c3afefe47fbdb56dc92c46
Oskar <jantaoskar@gmail.com>
2023-01-14 23:01:18 +0100

    2023_01_14_02

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -17,6 +17,32 @@ def fizz_buzz(number: int) -> str:
     else:
         return str(number)
 
+# my solution
+input("Play Fizz Buzz.   Press ENTER to start")
+print()
 
-for i in range(1, 101):
+for i in range(1, 102, 2):
+    if i == 101:
+        print("Congratulations - you win")
+        break
     print(fizz_buzz(i))
+    print("Your turn")
+    guess = input()
+    if guess != fizz_buzz(i+1):
+        print("Wrong - you lose, the correct answer was {}".format(fizz_buzz(i+1)))
+        break
+
+
+# Tim solution
+# next_number = 0
+# while next_number < 99:
+#     next_number += 1
+#     print(fizz_buzz(next_number))
+#     next_number += 1
+#     correct_answer = fizz_buzz(next_number)
+#     players_answer = input("Your go: ")
+#     if players_answer != correct_answer:
+#         print("Wrong - you lose, the correct answer was {}".format(fizz_buzz(correct_answer)))
+#         break
+# else:
+#     print("Well done, you reached {}".format(next_number))
